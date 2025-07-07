ecab45b7c07f2c246d832cb0bd2b2da8dd758132
scot <riftedkris@gmail.com>
2025-07-07 03:03:17 +0300

    SRCCode Updated
    Source Code

--- Source/sourcecode.lua
+++ Source/sourcecode.lua
@@ -1,25 +1,73 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+-- GUI Library
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Window = OrionLib:MakeWindow({Name = "Fun Scripts", HidePremium = false, SaveConfig = false, ConfigFolder = "FScripts"})
-local CreditsTab = Window:MakeTab({
-	Name = "Credits",
-	Icon = "rbxassetid://15771263462",
-	PremiumOnly = false
+-- Rayfield Window
+
+local Window = Rayfield:CreateWindow({
+   Name = "FScripts",
+   Icon = 111948200153431,
+   LoadingTitle = "FScripts",
+   LoadingSubtitle = "by scot",
+   ShowText = "FS",
+   Theme = "Default",
+
+   ToggleUIKeybind = "L",
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = FScripts, -- Create a custom folder for your hub/game
+      FileName = "config"
+   },
+
+   KeySystem = false,
+      KeySettings = {
+      Title = "Key System",
+      Subtitle = "FScript Key System",
+      Note = "Isn't available",
+      FileName = "keylock",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"robinho"}
+}})
+
+-- Notifier
+
+Rayfield:Notify({
+   Title = "Warning",
+   Content = "Some scripts are dead, buggy or discontinued. Make a ticket in the discord if so.",
+   Duration = 6.5,
+   Image = 4483362458,
 })
 
-CreditsTab:AddLabel("Credits to: sc0t6 (scot) for making the script.")
+-- Tabs
 
-local MainTab = Window:MakeTab({
-        Name = "Main"
-        Icon = "rbxassestid://1108967379"
+local MiscTab = Window:CreateTab("Misc", 12120687742)
+
+local MainTab = Window:CreateTab("PC Scripts", 12684119225)
+
+-- Labels
+
+local Label = MainTab:CreateLabel("Menu Keybind: L")
+
+-- Buttons
+
+local SelfFlingButton = MainTab:CreateButton({
+   Name = "Self-Fling",
+   Callback = function(v)
+   v = loadstring(game:HttpGet("https://raw.githubusercontent.com/sc0t6/FScripts/refs/heads/main/Scripts/Self-Fling.lua",true))()
+   end,
 })
 
-MainTab:AddButton({
-	Name = "Self-Fling",
-	Callback = function()
-      		loadstring(game:HttpGet("https://raw.githubusercontent.com/sc0t6/FScripts/refs/heads/main/Scripts/Self-Fling.lua",true))()
-  	end    
+local PrintBtn = MainTab:CreateButton({
+   Name = "test print",
+   Callback = function(b)
+   b = print('hello')
+   end,
 })
 
-end
-OrionLib:Init()
+local UnloadBtn = MiscTab:CreateButton({
+   Name = "Unload",
+   Callback = function(l)
+   l = Rayfield:Destroy()
+   end,
+})
